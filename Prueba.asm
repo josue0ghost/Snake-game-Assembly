39b07bf41f9fe5b9ef6c7a65907518e876865974
Javier Mollinedo <35619690+JMollinedo@users.noreply.github.com>
2019-10-09 17:46:46 -0600

    Ingresos de Parametros 02

--- Prueba.asm
+++ Prueba.asm
@@ -1,17 +1,10 @@
 .MODEL  small
 .DATA   ;segmento de datos
-    px1 DB  ?   ;posicionX serpiente1
-    py1 DB  ?   ;posicionY serpiente1
-    px2 DB  ?   ;posicionX serpiente2
-    py2 DB  ?   ;posicionY serpiente2
-    px3 DB  ?   ;posicionX serpiente3
-    py3 DB  ?   ;posicionY serpiente3
-    px4 DB  ?   ;posicionX serpiente4
-    py4 DB  ?   ;posicionY serpiente4
     dimX    DB  ?   ;dimensionX
     dimY    DB  ?   ;dimensionX
     mpX DB  'ingrese dimension horizontal$'
     mpY DB  'ingrese dimension vertical$'
+    minsj DB 'W = arriba, S = abajo, D = derecha, A = izquierda (DESACTIVE MAYUS)$'
     mer1 DB  'No se puede regresar$'
     mer2 DB  'Fin de Juego$'
 .STACK  ;segmento de pila
@@ -34,17 +27,9 @@ programa:   ;inicio de programa
     MOV AH , 01h    ;AL = teclado
     INT 21h
     SUB AL , 30h    ;ascii->digito
-    MOV px1 , AL    ;px1 = AL
-    SUB AL , 1  ;AL = AL - 1
-    MOV px2 , AL    ;px2 = AL
-    SUB AL , 1  ;AL = AL - 1
-    MOV px3 , AL    ;px3 = AL
-    SUB AL , 1  ;AL = AL - 1
-    MOV px4 , AL    ;px4 = AL
-    ADD AL , 3  ;AL = AL + 3
     MOV dimX , AL   ;dimX = AL
     ADD AL , dimX   ;dimX = dimX + dimX
-    SUB AL , 1  ;dimX = dimX - 1
+    INC AL  ;dimX = dimX + 1
     MOV dimX , AL
     MOV DL , 13 ;char de fin de linea
     MOV AH , 02h 
@@ -67,13 +52,9 @@ programa:   ;inicio de programa
     MOV AH , 01h    ;AL = teclado
     INT 21h
     SUB AL , 30h    ;ascii->digito
-    MOV py1 , AL   ;py1 = AL
-    MOV py2 , AL   ;py2 = AL
-    MOV py3 , AL   ;py3 = AL
-    MOV py4 , AL   ;py4 = AL
     MOV dimY , AL   ;dimY = AL
     ADD AL , dimY   ;dimY = dimY + dimY
-    SUB AL , 1  ;dimY = dimY - 1
+    INC AL ;dimY = dimY + 1
     MOV dimY , AL
     MOV DL , 13 ;char de fin de linea
     MOV AH , 02h 
@@ -82,7 +63,17 @@ programa:   ;inicio de programa
     MOV AH , 02h 
     INT 21h ;imprime caracter
     
-    
+    ;ingresar dimension horizontal
+    XOR DX , DX ;limpiar registro
+    MOV DX , OFFSET minsj ;prepara mpX para imprimir
+    MOV AH , 09h
+    INT 21h ;imprime mpX
+    MOV DL , 13 ;char de fin de linea
+    MOV AH , 02h 
+    INT 21h ;imprime caracter
+    MOV DL , 10 ;char de salto de linea
+    MOV AH , 02h 
+    INT 21h ;imprime caracter
     
     JMP fin_programa    ;salta a la etiqueta fin_programa
 
