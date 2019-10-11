a0ac10a8298ebca351403ebf141ede49cd9c0c2f
josue0ghost <36039766+josue0ghost@users.noreply.github.com>
2019-10-11 15:58:25 -0600

    snake es el mero mero

--- Prueba.asm
+++ Prueba.asm
@@ -1,37 +1,32 @@
-.MODEL  medium
-.DATA   ;segmento de datos
+.MODEL  small
+.STACK
+.DATA   
     dimX    DB  ?   ;dimensionX
     dimY    DB  ?   ;dimensionX
-    mpX DB  'ingrese dimension horizontal$'
-    mpY DB  'ingrese dimension vertical$'
+    mpX DB  'ingrese dimension horizontal: $'
+    mpY DB  'ingrese dimension vertical: $'
     minsj DB 'W = arriba, S = abajo, D = derecha, A = izquierda (DESACTIVE MAYUS)$'
     mer1 DB  'No se puede regresar$'
     mer2 DB  'Fin de Juego$'
     
-    ;posiciones snake
-    x1 DB ?
-    y1 DB ?
-    x2 DB ?
-    y2 DB ?
-    x3 DB ?
-    y3 DB ?
-    x4 DB ?
-    y4 DB ?
-    
     ; caracteres
-    barrera DB 23h  ; #
+    barrera DB 178d  ; #
     cuerpo DB 4fh   ; O
     fruta DB 40h    ; @
    
-.STACK  ;segmento de pila
+
 .CODE
 program:
 
-mainprogram proc far   ;inicio de programa
-    MOV AX , @DATA  ;guardar direccion de segmento de datos
-    MOV DS , AX
+   ;inicio de programa
+    MOV AX, @DATA  ;guardar direccion de segmento de datos
+    MOV DS, AX
     
     call ingresodata
+    
+    mov ax, 0003h   ;limpia la pantalla
+    int 10h
+    
     call lineaH
     
     mov dl, 0dh
@@ -42,80 +37,93 @@ mainprogram proc far   ;inicio de programa
     
     mov bl, dimY
     printVertical:
-    
         call lineaV
         dec bl
-    
     jnz printVertical
+    
     call lineaH
-    call fin_programa
+
     
-    
-    INGRESODATA proc
-    ;ingresar dimension horizontal
-    XOR DX , DX ;limpiar registro
-    MOV DX , OFFSET mpX    ;prepara mpX para imprimir
+
+    fin_juego:   
+    XOR DX , DX             ;limpiar registro
+    MOV DL , OFFSET mer2    ;prepara mer2 para imprimir
     MOV AH , 09h
-    INT 21h ;imprime mpX
-    MOV DL , 13 ;char de fin de linea
-    MOV AH , 02h 
-    INT 21h ;imprime caracter
-    MOV DL , 10 ;char de salto de linea
-    MOV AH , 02h 
-    INT 21h ;imprime caracter
-    MOV AH , 01h    ;AL = teclado
+    INT 21h                 ;imprime mer2
+    
+    fin_programa:
+    ;instruccion de fin de programa
+    MOV AH, 4Ch
     INT 21h
-    SUB AL , 30h    ;ascii->digito
-    MOV dimX , AL   ;dimX = AL
-    ADD AL , dimX   ;dimX = dimX + dimX
-    INC AL  ;dimX = dimX + 1
-    MOV dimX , AL
-    MOV DL , 13 ;char de fin de linea
-    MOV AH , 02h 
-    INT 21h ;imprime caracter
-    MOV DL , 10 ;char de salto de linea
-    MOV AH , 02h 
-    INT 21h ;imprime caracter
+    
+    
+    
+    ingresodata proc
+    ;ingresar dimension horizontal
+    XOR DX, DX              ;limpiar registro
+    MOV DL, OFFSET mpX      ;prepara mpX para imprimir
+    MOV AH, 09h
+    INT 21h                 ;imprime mpX
+    MOV DL, 13d             ;char de fin de linea
+    MOV AH, 02h 
+    INT 21h                 ;imprime caracter
+    MOV DL, 10d             ;char de salto de linea
+    MOV AH, 02h 
+    INT 21h                 ;imprime caracter
+    
+    MOV AH, 01h             ;AL = teclado
+    INT 21h
+    SUB AL, 30h             ;ascii->digito
+    MOV dimX, AL            ;dimX = AL
+    ADD AL, dimX            ;dimX = dimX + dimX
+    INC AL                  ;dimX = dimX + 1
+    MOV dimX, AL
+    MOV DL, 13d             ;char de fin de linea
+    MOV AH, 02h 
+    INT 21h                 ;imprime caracter
+    MOV DL, 10d             ;char de salto de linea
+    MOV AH, 02h 
+    INT 21h                 ;imprime caracter
     
     ;ingresar dimension vertical
-    XOR DX , DX ;limpiar registro
-    MOV DX , OFFSET mpY    ;prepara mpY para imprimir
-    MOV AH , 09h
-    INT 21h ;imprime mpY
-    MOV DL , 13 ;char de fin de linea
-    MOV AH , 02h 
-    INT 21h ;imprime caracter
-    MOV DL , 10 ;char de salto de linea
-    MOV AH , 02h 
-    INT 21h ;imprime caracter
-    MOV AH , 01h    ;AL = teclado
+    XOR DX, DX              ;limpiar registro
+    MOV DL, OFFSET mpY      ;prepara mpY para imprimir
+    MOV AH, 09h
+    INT 21h                 ;imprime mpY
+    MOV DL, 13d             ;char de fin de linea
+    MOV AH, 02h 
+    INT 21h                 ;imprime caracter
+    MOV DL, 10d             ;char de salto de linea
+    MOV AH, 02h 
+    INT 21h                 ;imprime caracter
+    MOV AH, 01h             ;AL = teclado
     INT 21h
-    SUB AL , 30h    ;ascii->digito
-    MOV dimY , AL   ;dimY = AL
-    ADD AL , dimY   ;dimY = dimY + dimY
-    INC AL ;dimY = dimY + 1
-    MOV dimY , AL
-    MOV DL , 13 ;char de fin de linea
+    SUB AL, 30h             ;ascii->digito
+    MOV dimY, AL            ;dimY = AL
+    ADD AL, dimY            ;dimY = dimY + dimY
+    INC AL                  ;dimY = dimY + 1
+    MOV dimY, AL
+    MOV DL, 13d             ;char de fin de linea
+    MOV AH, 02h 
+    INT 21h 
+    MOV DL, 10d             ;char de salto de linea
     MOV AH , 02h 
-    INT 21h ;imprime caracter
-    MOV DL , 10 ;char de salto de linea
-    MOV AH , 02h 
-    INT 21h ;imprime caracter
+    INT 21h 
     
-    ;ingresar dimension horizontal
-    XOR DX , DX ;limpiar registro
-    MOV DX , OFFSET minsj ;prepara mpX para imprimir
+    ;instrucciones
+    XOR DX, DX            
+    MOV DL, OFFSET minsj 
     MOV AH , 09h
-    INT 21h ;imprime mpX
-    MOV DL , 13 ;char de fin de linea
-    MOV AH , 02h 
-    INT 21h ;imprime caracter
-    MOV DL , 10 ;char de salto de linea
-    MOV AH , 02h 
-    INT 21h ;imprime caracter
+    INT 21h     
+    MOV DL, 13d             ;char de fin de linea
+    MOV AH, 02h 
+    INT 21h                 ;imprime caracter
+    MOV DL, 10d             ;char de salto de linea
+    MOV AH, 02h 
+    INT 21h                 ;imprime caracter
     
     ret
-    INGRESODATA endp
+    ingresodata endp
     
     lineaH proc
         xor cx, cx
@@ -164,20 +172,5 @@ mainprogram proc far   ;inicio de programa
         
         ret
     lineaV endp
-    
-    
-    
-    ;JMP fin_programa    ;salta a la etiqueta fin_programa
-
-    fin_programa proc   ;etiqueta fin_programa
-    XOR DX , DX ;limpiar registro
-    MOV DX , OFFSET mer2    ;prepara mer2 para imprimir
-    MOV AH , 09h
-    INT 21h ;imprime mer2
-    ;instruccion de fin de programa
-    MOV AH, 4Ch
-    INT 21h
-    fin_programa endp
-mainprogram endp
 
 end program
